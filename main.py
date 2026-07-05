91bca90a85a5b4edf483caaf4b0e70b17dfcacf1
Harshit Singh <harshitsingh6405@gmail.com>
2026-07-06 00:08:48 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+#The making of GUI of the application is done in this file. The main window of the application is created here.
+
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QWidget
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setGeometry(700, 400, 500, 500)
+
+def main():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+
+
+
+if __name__ == "__main__":
+    main()
